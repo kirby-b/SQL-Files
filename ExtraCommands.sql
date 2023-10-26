0e8b35688aa55dc4e5aa3614ab60716de9fc6436
kirby-b <kirby.brodie66@gmail.com>
2023-10-25 20:23:07 -0400

    Added more commands as part of family guy table
    -added Drop tables, Delete from tables, delete from table columns, insert into table, and replace value commands
    -Also fixed minor typos and adds rows with labels for understanding

---
+++ ExtraCommands.sql
@@ -0,0 +1,24 @@
+--Family Guy Table:
+
+--Deletes Tables
+DROP TABLE Jobs;
+DROP TABLE Dead_Characters;
+
+--Deletes all data from the tables but keeps tables
+DELETE FROM Jobs;
+DELETE FROM Dead_Characters;
+
+--Deletes a specific row, replace the values in the parentheses with the specified values
+DELETE FROM Jobs WHERE lastName='LastNameOfCharacter' AND firstName='FirstNameOfCharacter'; 
+DELETE FROM Dead_Characters WHERE lastName='LastNameOfCharacter' AND firstName='FirstNameOfCharacter'; 
+
+--Inserts a new value into the table
+INSERT INTO Jobs (LastName, FirstName, Job, Began, Pay)
+VALUES ('LastName', 'FirstName', 'Job', 'Began', 'Pay');
+INSERT INTO Dead_Characters (LastName, FirstName, End_Age, Death_Date, On_Screen)
+VALUES ('LastName', 'FirstName', 'End_Age', 'Death_Date', 'On_Screen');
+
+--Replace a value
+UPDATE Jobs SET chosenValue = 'newValue' WHERE idNum = 'idNum';
+UPDATE Dead_Characters SET chosenValue = 'newValue' WHERE idNum = 'idNum';
+UPDATE Jobs SET Job = 'Fisherman' WHERE idNum = '0';--Example
--- Family-Guy.sql
+++ Family-Guy.sql
@@ -18,30 +18,32 @@ CREATE TABLE Dead_Characters (
     On_Screen VARCHAR(5)
 );
 
-INSERT INTO Jobs (LastName, FirstName, Job, Began, Pay)
+INSERT INTO Jobs (idNum, LastName, FirstName, Job, Began, Pay)
 VALUES 
-('Griffin','Peter','Brewery Worker','2023-02-15','35.00'), 
-('Griffin','Lois','Piano Teacher','1999-01-31','10.00'), 
-('Grffin','Chris','Paper Boy','2001-11-15','8.00'), 
-('Griffin','Megatron','Freelancer','2001-01-01','20.00'), 
-('Griffin','Brian','Failed Writer','1999-01-31','0.00'), 
-('Stewie','Griffin','Inventor','1999-01-31','200.00'), 
-('Swanson','Joe','Cop','1999-05-02','23.86'), 
-('Quagmire','Glen','Pilot','1989-01-31','36.91'), 
-('Brown','Clevland','Deli Worker','2014-10-19','30.00'), 
-('Pewterschmidt','Carter','Billionare','1927-07-29','9999.99'), 
-('West','Adam','Mayor','1999-01-31','30.37'), 
-('Goldman','Mort','Pharmacist','1983-05-28','40.00');
+('idNum', 'LastName', 'FirstName', 'Job', 'Began', 'Pay'),
+('0', 'Griffin','Peter','Brewery Worker','2023-02-15','35.00'), 
+('1', 'Griffin','Lois','Piano Teacher','1999-01-31','10.00'), 
+('2', 'Grffin','Chris','Paper Boy','2001-11-15','8.00'), 
+('3', 'Griffin','Megatron','Freelancer','2001-01-01','20.00'), 
+('4', 'Griffin','Brian','Failed Writer','1999-01-31','0.00'), 
+('5', 'Griffin','Stewie','Inventor','1999-01-31','200.00'), 
+('6', 'Swanson','Joe','Cop','1999-05-02','23.86'), 
+('7', 'Quagmire','Glen','Pilot','1989-01-31','36.91'), 
+('8', 'Brown','Clevland','Deli Worker','2014-10-19','30.00'), 
+('9', 'Pewterschmidt','Carter','Billionare','1927-07-29','9999.99'), 
+('10', 'West','Adam','Mayor','1999-01-31','30.37'), 
+('11', 'Goldman','Mort','Pharmacist','1983-05-28','40.00');
 
-INSERT INTO Jobs (LastName, FirstName, End_Age, Death_Date, On_Screen)
+INSERT INTO Dead_Characters (idNum, LastName, FirstName, End_Age, Death_Date, On_Screen)
 VALUES 
-('Unknown','Angela','About 40','2018-12-09','No'),
-('Goldman','Muriel','42','2010-09-26','Yes'),
-('Brown','Loretta','About 40','2010-04-11','Yes'),
-('West','Adam','88','2017-06-09','No'),
-('Unknown','Bertram','About 1','2011-05-08','Yes'),
-('Simmons','Diane','40','2010-09-26','Yes'),
-('Griffin','Francis','80','2011-02-11','Yes'),
-('Weed','Jonathan','About 40','2000-10-13','Yes'),
-('Griffin','Thelma','82','2014-03-09','No');
+('idNum', 'LastName', 'FirstName', 'End_Age', 'Death_Date', 'On_Screen'),
+('0', 'Unknown','Angela','About 40','2018-12-09','No'),
+('1', 'Goldman','Muriel','42','2010-09-26','Yes'),
+('2', 'Brown','Loretta','About 40','2010-04-11','Yes'),
+('3', 'West','Adam','88','2017-06-09','No'),
+('4', 'Unknown','Bertram','About 1','2011-05-08','Yes'),
+('5', 'Simmons','Diane','40','2010-09-26','Yes'),
+('6', 'Griffin','Francis','80','2011-02-11','Yes'),
+('7', 'Weed','Jonathan','About 40','2000-10-13','Yes'),
+('8', 'Griffin','Thelma','82','2014-03-09','No');
 
